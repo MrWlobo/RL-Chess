8f7e928d3ca4f386205e1be8a207a6bbb485bc66
endzju <andrebaronstudent@gmail.com>
2026-05-16 00:21:10 +0200

    comments

--- src/rl_chess/utils/MonteCarloTreeSearch.py
+++ src/rl_chess/utils/MonteCarloTreeSearch.py
@@ -14,7 +14,7 @@ class MCTSNode:
     ):
         self.state = state  # Stan planszy (np. fen)
         self.parent = parent
-        self.children = {}  # Słownik: {move: MCTSNode}
+        self.children: dict[chess.Move, MCTSNode] = {}
         self.n = 0  # Liczba odwiedzin (N)
         self.q = 0  # Średnia wartość (Q)
         self.p = prior_p  # Prawdopodobieństwo z sieci (P)
@@ -48,7 +48,6 @@ class MonteCarloTreeSearch:
                 node = root
                 search_path = [node]
 
-                # 1. SELEKCJA
                 while node.children:
                     move, node = max(
                         node.children.items(),
@@ -56,19 +55,15 @@ class MonteCarloTreeSearch:
                     )
                     search_path.append(node)
 
-                # 2. EKSPANSJA I EWALUACJA (Twój ResNet)
-                # Przygotuj tensor dla sieci
                 self.temp_board.set_fen(node.state)
                 policy, value = neural_network(
                     board_to_tensor(board=self.temp_board, device=device)
                 )
                 policy = policy.cpu()
 
-                # Stwórz dzieci dla wszystkich legalnych ruchów
                 legal_moves = self.temp_board.legal_moves
 
                 for move in legal_moves:
-                    # Pobierz p dla tego ruchu z wyjścia sieci (głowa Policy)
                     p_move = policy[0][move_to_index(move)]
                     self.temp_board.push(move)
                     node.children[move] = MCTSNode(
@@ -80,7 +75,6 @@ class MonteCarloTreeSearch:
                     f"MCTS cycle {i + 1} before backpropagation, time: {time.time() - tic:.5f}"
                 )
 
-                # 3. BACKPROPAGATION
                 self.backpropagate(search_path, value)
                 print(
                     f"MCTS cycle {i + 1} completed, time: {time.time() - tic:.5f}"
@@ -91,8 +85,7 @@ class MonteCarloTreeSearch:
         return max(root.children.items(), key=lambda x: x[1].n)[0]
 
     def backpropagate(self, path: list[MCTSNode], value: float):
-        # Pamiętaj o naprzemienności: wartość dla przeciwnika jest ujemna
         for node in reversed(path):
             node.q = (node.n * node.q + value) / (node.n + 1)
             node.n += 1
-            value = -value  # Zmiana perspektywy gracza
+            value = -value
